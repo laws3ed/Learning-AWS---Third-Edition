0cd41deae4c08e8b451097a59344db65dab79eae
laws3ed <138744019+laws3ed@users.noreply.github.com>
2023-10-18 16:32:07 +0530

    Add files via upload

---
+++ ch2/data_preparation_create_input_parquet_gzipped_files.py
@@ -0,0 +1,33 @@
+from dask import dataframe as dd
+import pandas as pd
+import numpy as np
+import os
+import time
+from datetime import datetime
+
+data_prep_start = time.time()
+print("Data preparation start time: ", datetime.now())
+
+df1 = pd.read_csv('/work/data/csv_dir/sample_dataset_1.csv')
+df1['event_time1'] = pd.to_datetime(df1['event_time1'])
+df1=df1.sort_values(by=['event_time1'])
+
+df2 = pd.read_csv('/work/data/csv_dir/sample_dataset_2.csv')
+df2['event_time2'] = pd.to_datetime(df2['event_time2'])
+#df2.set_index('event_time2', inplace=True)
+df2=df2.sort_values(by=['event_time2'])
+
+#convert to parquet format with 16 partitions
+ddf1 = dd.from_pandas(df1, npartitions=16)
+ddf1.to_parquet("/work/data/parquet_gzipped_dir_1/", write_index=False, engine='fastparquet', times='int96', overwrite=True, compression="gzip")
+print("Listing the contents of the parquet file directory 1:")
+print(os.listdir('/work/data/parquet_gzipped_dir_1'))
+
+ddf2 = dd.from_pandas(df2, npartitions=8)
+ddf2.to_parquet("/work/data/parquet_gzipped_dir_2/", write_index=False, engine='fastparquet', times='int96', overwrite=True, compression="gzip")
+print("Listing the contents of the parquet file directory 2:")
+print(os.listdir('/work/data/parquet_gzipped_dir_2'))
+
+data_prep_end = time.time()
+print("Data preparation end time: ", datetime.now())
+print("Time taken for data preparation: ",(data_prep_end - data_prep_start),"sec")
